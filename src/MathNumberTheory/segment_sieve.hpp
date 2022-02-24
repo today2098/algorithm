48213dac2f1ec7b7a7fea24f3949a4c1a058e7e6
today2098 <ipp.10032018@gmail.com>
2022-02-24 12:53:09 +0900

    [add] ファイル"segment_sieve.hpp"を作成

---
+++ src/MathNumberTheory/segment_sieve.hpp
@@ -0,0 +1,88 @@
+#ifndef ALGORITHM_SEGMENT_SIEVE_HPP
+#define ALGORITHM_SEGMENT_SIEVE_HPP 1
+
+#include <algorithm>
+#include <cassert>
+#include <cmath>
+#include <map>
+#include <vector>
+
+namespace algorithm {
+
+// 区間篩．
+class SegmentSieve {
+    long long l, r, m;
+    std::vector<long long> small;                  // small[n]:=(範囲[2,√r)の自然数xのある素因数).
+    std::vector<std::map<long long, int> > large;  // large[n-l]:=(範囲[l,r)の自然数xのいくつかの素因数).
+    std::vector<long long> aux;                    // aux[n-l]:=(large[n-l]の素因数の積).
+
+    void build() {
+        m = std::sqrt(r) + 1;
+        small.assign(m, -1LL);
+        for(long long i = 2; i < m; ++i) small[i] = i;
+        large.resize(r - l);
+        aux.assign(r - l, 1LL);
+        for(long long i = 2; i * i < r; ++i) {
+            if(small[i] == i) {
+                for(long long j = i * i; j < m; j += i) small[j] = i;
+                for(long long j = std::max<long long>(2LL, (l + i - 1) / i) * i; j < r; j += i) {
+                    long long tmp = j;
+                    while(tmp % i == 0 and aux[j - l] * aux[j - l] <= r) {
+                        large[j - l][i]++;
+                        aux[j - l] *= i;
+                        tmp /= i;
+                    }
+                }
+            }
+        }
+    }
+
+public:
+    // constructor. 制約の目安は大体2<=l<r<=1e12, r-l<=1e6．
+    SegmentSieve() : SegmentSieve(2, 3) {}
+    explicit SegmentSieve(long long l_, long long r_) : l(l_), r(r_) {  // 範囲[l,r)の自然数を篩にかける．
+        assert(2LL <= l and l < r);
+        build();
+    }
+
+    bool is_prime(long long n) const {  // 素数判定．O(1).
+        assert(l <= n and n < r);
+        return large[n - l].size() == 0;
+    }
+    std::map<long long, int> prime_factorize(long long n) const {  // 高速素因数分解．
+        assert(l <= n and n < r);
+        std::map<long long, int> res = large[n - l];
+        n /= aux[n - l];
+        if(n >= m) {
+            res[n]++;
+            return res;
+        }
+        while(n > 1LL) {
+            res[small[n]]++;
+            n /= small[n];
+        }
+        return res;
+    }
+    std::vector<long long> divisors(long long n) const {  // 高速約数列挙．
+        assert(l <= n and n < r);
+        std::vector<long long> res({1LL});
+        if(n == 1) return res;
+        const auto &&pf = prime_factorize(n);
+        for(const auto &[p, cnt] : pf) {
+            int m = res.size();
+            for(int i = 0; i < m; ++i) {
+                long long v = 1;
+                for(int j = 0; j < cnt; ++j) {
+                    v *= p;
+                    res.push_back(res[i] * v);
+                }
+            }
+        }
+        std::sort(res.begin(), res.end());
+        return res;
+    }
+};
+
+}  // namespace algorithm
+
+#endif  // ALGORITHM_SEGMENT_SIEVE_HPP
