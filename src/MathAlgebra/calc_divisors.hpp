785f18d0576eaa222b4415b163b385868c6963f3
today2098 <ipp.10032018@gmail.com>
2022-02-25 17:13:08 +0900

    [add] ファイル"calc_divisors"の作成

---
+++ src/MathAlgebra/calc_divisors.hpp
@@ -0,0 +1,27 @@
+#ifndef ALGORITHM_DIVISOR_HPP
+#define ALGORITHM_DIVISOR_HPP 1
+
+#include <algorithm>
+#include <cassert>
+#include <vector>
+
+namespace algorithm {
+
+template <typename Type>
+std::vector<Type> calc_divisors(Type n) {  // 約数列挙．O(√N).
+    assert(0 <= n);
+    std::vector<Type> res;  // res[]:=(自然数nの約数の集合).
+    for(Type p = 1; p * p <= n; ++p) {
+        if(n % p == 0) {
+            res.push_back(p);
+            Type q = n / p;
+            if(q != p) res.push_back(q);
+        }
+    }
+    std::sort(res.begin(), res.end());
+    return res;
+}
+
+}  // namespace algorithm
+
+#endif  // ALGORITHM_DIVISOR_HPP
