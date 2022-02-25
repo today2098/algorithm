22f9bda1ce726801aac5ba19ce1aae8463d867a1
today2098 <ipp.10032018@gmail.com>
2022-02-25 17:35:20 +0900

    [add] ファイル"mod_inv.hpp"を作成

---
+++ src/MathCombinatorics/mod_inv.hpp
@@ -0,0 +1,22 @@
+#ifndef ALGORITHM_MOD_INV_HPP
+#define ALGORITHM_MOD_INV_HPP 1
+
+#include <cassert>
+#include <utility>
+
+namespace algorithm {
+
+long long mod_inv(long long a, int m) {  // 逆元．a^-1 (mod m)を求める（ただし，aとmは互いに素）．O(logA).
+    assert(1 <= m);
+    long long b = m, u = 1, v = 0;
+    while(b) {
+        long long t = a / b;
+        a -= t * b, u -= t * v;
+        std::swap(a, b), std::swap(u, v);
+    }
+    return u = (u % m + m) % m;
+}
+
+}  // namespace algorithm
+
+#endif  // ALGORITHM_MOD_INV_HPP
