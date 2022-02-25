f05dc54cc617adf61e9a4f5841aed667dfdbd15b
today2098 <ipp.10032018@gmail.com>
2022-02-26 01:05:30 +0900

    [add] ファイル"split.hpp"を作成

---
+++ src/String/split.hpp
@@ -0,0 +1,22 @@
+#ifndef ALGORITHM_SPLIT_HPP
+#define ALGORITHM_SPLIT_HPP 1
+
+#include <sstream>
+#include <string>
+#include <vector>
+
+namespace algorithm {
+
+std::vector<std::string> split(const std::string &s, char delim) {  // 文字列分割．指定の文字delimで分割する．
+    std::vector<std::string> res;
+    std::stringstream ss(s);
+    std::string item;
+    while(std::getline(ss, item, delim)) {
+        if(!item.empty()) res.push_back(item);
+    }
+    return res;
+}
+
+}  // namespace algorithm
+
+#endif  // ALGORITHM_SPLIT_HPP
