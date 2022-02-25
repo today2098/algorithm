ee31435563c0dc21252863d701c591d3f72166df
today2098 <ipp.10032018@gmail.com>
2022-02-25 23:39:41 +0900

    [add] ファイル"string_modulo.hpp"を作成

---
+++ src/String/string_modulo.hpp
@@ -0,0 +1,21 @@
+#ifndef ALGORITHM_STRING_MODULO_HPP
+#define ALGORITHM_STRING_MODULO_HPP 1
+
+#include <cassert>
+#include <string>
+
+namespace algorithm {
+
+long long string_modulo(const std::string &s, int m) {  // O(|S|).
+    int n = s.size();
+    long long res = 0LL;
+    for(int i = 0; i < n; ++i) {
+        assert('0' <= s[i] and s[i] <= '9');
+        res = (10 * res + (s[i] - '0')) % m;
+    }
+    return res;
+}
+
+}  // namespace algorithm
+
+#endif  // ALGORITHM_STRING_MODULO_HPP
