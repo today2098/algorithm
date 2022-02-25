331ff09d4df173b17315e766eb68889e14824fbd
today2098 <ipp.10032018@gmail.com>
2022-02-25 16:45:26 +0900

    [add] ファイル"ipow.hpp"を作成

---
+++ src/MathAlgebra/ipow.hpp
@@ -0,0 +1,18 @@
+#ifndef ALGORITHM_IPOW_HPP
+#define ALGORITHM_IPOW_HPP 1
+
+#include <cassert>
+
+namespace algorithm {
+
+long long ipow(long long n, long long k) {  // 繰返し二乗法．O(logK).
+    assert(0 <= k);
+    if(k == 0) return 1;
+    long long res = ipow(n * n, k >> 1);
+    if(k & 1LL) res *= n;
+    return res;
+}
+
+}  // namespace algorithm
+
+#endif  // ALGORITHM_IPOW_HPP
