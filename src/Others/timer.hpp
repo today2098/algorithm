fdb0a99876fb7a76868f949840efe095b8fabd35
Today <ipp.30042022@gmail.com>
2022-07-05 17:05:58 +0900

    [add] create a file "timer.hpp"

---
+++ src/Others/timer.hpp
@@ -0,0 +1,24 @@
+#ifndef ALGORITHM_TIMER_HPP
+#define ALGORITHM_TIMER_HPP 1
+
+#include <chrono>
+
+namespace algorithm {
+
+class Timer {
+    std::chrono::system_clock::time_point m_start;
+
+public:
+    // constructor.
+    Timer() { restart(); }
+
+    void restart() { m_start = std::chrono::system_clock::now(); }  // 計測開始時間を保存する．
+    double elapsed() const {                                        // 経過時間を返す（ミリ秒単位）．
+        auto end = std::chrono::system_clock::now();
+        return std::chrono::duration_cast<std::chrono::milliseconds>(end - m_start).count();
+    }
+};
+
+}  // namespace algorithm
+
+#endif  // ALGORITHM_TIMER_HPP
