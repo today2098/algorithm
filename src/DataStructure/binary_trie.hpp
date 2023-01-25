bd3c55ed50eee19f72bd26831c31549c623e3da8
Today <ipp.15122098@gmail.com>
2023-01-26 00:29:30 +0900

    [update] Specify number of elements to insert and erase

--- src/DataStructure/binary_trie.hpp
+++ src/DataStructure/binary_trie.hpp
@@ -8,78 +8,78 @@
 namespace algorithm {
 
 // 非負整数値を扱うTrie木．
-template <typename T = unsigned, int B = 32>  // T:データ型, B:ビット長.
+template <typename T = unsigned, int B = 32, typename U = size_t>  // T:非負整数値の型, B:ビット長, U:サイズの型.
 class BinaryTrie {
     struct Node {
-        int cnt;      // cnt:=(自身を根とする部分木に含まれる要素数).
+        U cnt;        // cnt:=(自身を根とする部分木に含まれる要素数).
         Node *ch[2];  // ch[]:=(子のポインタ). 二分木．
         Node() : cnt(0), ch({nullptr, nullptr}) {}
     };
 
     Node *root;  // root:=(根のポインタ).
 
-    void delete_tree(Node *rt) {
+    void delete_subtree(Node *rt) {
         if(rt == nullptr) return;
-        for(bool flag : {0, 1}) {
-            delete_tree(rt->ch[flag]);
-            rt->ch[flag] = nullptr;
+        for(bool bit : {0, 1}) {
+            delete_subtree(rt->ch[bit]);
+            rt->ch[bit] = nullptr;
         }
         delete rt;
         rt = nullptr;
     }
-    Node *add(Node *p, T x, int shift = B - 1) {
+    Node *add(Node *p, T x, U cnt, int shift = B - 1) {  // Top down.
         if(p == nullptr) p = new Node;
-        p->cnt++;
+        p->cnt += cnt;
         if(shift < 0) return p;
-        bool flag = (x >> shift) & (T)1;
-        p->ch[flag] = add(p->ch[flag], x, shift - 1);
+        bool bit = (x >> shift) & (T)1;
+        p->ch[bit] = add(p->ch[bit], x, cnt, shift - 1);
         return p;
     }
-    Node *sub(Node *p, T x, int shift = B - 1) {
-        assert(p != nullptr);
-        p->cnt--;
+    Node *sub(Node *p, T x, U cnt, int shift = B - 1) {  // Top down.
+        assert(p != nullptr and p->cnt >= cnt);
+        p->cnt -= cnt;
         if(p->cnt == 0) {
-            delete_tree(p);
+            delete_subtree(p);
             return p = nullptr;
         }
         if(shift < 0) return p;
-        bool flag = (x >> shift) & (T)1;
-        p->ch[flag] = sub(p->ch[flag], x, shift - 1);
+        bool bit = (x >> shift) & (T)1;
+        p->ch[bit] = sub(p->ch[bit], x, cnt, shift - 1);
         return p;
     }
     T get_min(Node *p, T bias = 0, int shift = B - 1) const {
         assert(p != nullptr);
         if(shift < 0) return 0;
-        bool flag = (bias >> shift) & (T)1;
-        if(p->ch[flag] == nullptr) flag = !flag;
-        return get_min(p->ch[flag], bias, shift - 1) | ((T)flag << shift);
+        bool bit = (bias >> shift) & (T)1;
+        if(p->ch[bit] == nullptr) bit = !bit;
+        return get_min(p->ch[bit], bias, shift - 1) | ((T)bit << shift);
     }
-    T get(Node *p, int k, T bias = 0, int shift = B - 1) const {
+    T get(Node *p, U k, T bias = 0, int shift = B - 1) const {
         assert(p != nullptr);
         if(shift < 0) return 0;
-        bool flag = (bias >> shift) & (T)1;
-        int m = (p->ch[flag] == nullptr ? 0 : p->ch[flag]->cnt);
+        bool bit = (bias >> shift) & (T)1;
+        U m = (p->ch[bit] == nullptr ? 0 : p->ch[bit]->cnt);
         if(k < m) {
-            return get(p->ch[flag], k, bias, shift - 1) | ((T)flag << shift);
+            return get(p->ch[bit], k, bias, shift - 1) | ((T)bit << shift);
         } else {
-            flag = !flag;
-            return get(p->ch[flag], k - m, bias, shift - 1) | ((T)flag << shift);
+            bit = !bit;
+            return get(p->ch[bit], k - m, bias, shift - 1) | ((T)bit << shift);
         }
     }
-    int count_lower(Node *p, T x, int shift = B - 1) const {
+    U get_lower(Node *p, T x, int shift = B - 1) const {
         if(p == nullptr or shift < 0) return 0;
-        bool flag = (x >> shift) & (T)1;
-        return ((flag and p->ch[0] != nullptr) ? p->ch[0]->cnt : 0) + count_lower(p->ch[flag], x, shift - 1);
+        bool bit = (x >> shift) & (T)1;
+        return ((bit and p->ch[0] != nullptr) ? p->ch[0]->cnt : 0) + get_lower(p->ch[bit], x, shift - 1);
     }
     void dfs(std::ostream &os, Node *p, std::bitset<B> &bits, int shift = B - 1) const {
         if(shift < 0) {
             os << "[" << bits << "] (num: " << (T)bits.to_ullong() << ", cnt: " << p->cnt << ")\n";
             return;
         }
-        for(bool flag : {0, 1}) {
-            if(p->ch[flag] != nullptr) {
-                bits.set(shift, flag);
-                dfs(os, p->ch[flag], bits, shift - 1);
+        for(bool bit : {0, 1}) {
+            if(p->ch[bit] != nullptr) {
+                bits.set(shift, bit);
+                dfs(os, p->ch[bit], bits, shift - 1);
             }
         }
     }
@@ -93,7 +93,7 @@ public:
     }
 
     // 集合内でk番目に小さい値を取得する．O(B).
-    T operator[](int k) const {
+    T operator[](U k) const {
         assert(0 <= k and k < size());
         return get(root, k);
     }
@@ -101,9 +101,9 @@ public:
     // 要素が空かどうか判定する．O(1).
     bool empty() const { return root == nullptr; }
     // 要素数を返す．O(1).
-    int size() const { return (root == nullptr ? 0 : root->cnt); }
+    U size() const { return (root == nullptr ? 0 : root->cnt); }
     // 値xの要素数を返す．O(B).
-    int count(T x) const {
+    U count(T x) const {
         if(root == nullptr) return 0;
         Node *p = root;
         for(int i = B - 1; i >= 0; --i) {
@@ -112,29 +112,29 @@ public:
         }
         return p->cnt;
     }
-    // 集合に値xを1つ追加する．O(B).
-    void insert(T x) { root = add(root, x); }
-    // 集合から値xを1つ削除する．O(B).
-    void erace(T x) {
-        assert(count(x) >= 1);
-        root = sub(root, x);
+    // 集合に値xを追加する．O(B).
+    void insert(T x, U cnt = 1) { root = add(root, x, cnt); }
+    // 集合から値xを削除する．O(B).
+    void erase(T x, U cnt = 1) {
+        assert(count(x) >= cnt);
+        root = sub(root, x, cnt);
     }
     // 集合内で値xとXORしたときに最小となる値を取得する．O(B).
     T min_element(T x = 0) const { return get_min(root, x); }
     // 集合内で値xとXORしたときに最大となる値を取得する．O(B).
     T max_element(T x = 0) const { return get_min(root, ~x); }
     // 集合内で値xとXORしたときにk番目に小さい値を取得する．O(B).
-    T kth_element(int k, T x = 0) const {
+    T kth_element(U k, T x = 0) const {
         assert(0 <= k and k < size());
         return get(root, k, x);
     }
-    // 集合内で値x以上の最小の要素の番号を取得する．O(B).
-    int lower_bound(T x) const { return count_lower(root, x); }
-    // 集合内で値xより大きい最小の要素の番号を取得する．O(B).
-    int upper_bound(T x) const { return count_lower(root, x + 1); }
+    // 集合内で値x以上の最小の要素番号を取得する．O(B).
+    U lower_bound(T x) const { return get_lower(root, x); }
+    // 集合内で値xより大きい最小の要素番号を取得する．O(B).
+    U upper_bound(T x) const { return get_lower(root, x + 1); }
     // 全要素を削除する．
     void clear() {
-        delete_tree(root);
+        delete_subtree(root);
         root = nullptr;
     }
 
