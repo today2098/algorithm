1c888ca5088155a5e8ff7430467f4512a2af134f
Today <ipp.15122098@gmail.com>
2023-05-30 11:52:38 +0900

    [clean] Refactoring "BIT2D" and "BIT2D0"

--- src/DataStructure/binary_indexed_tree_2d.hpp
+++ src/DataStructure/binary_indexed_tree_2d.hpp
@@ -10,35 +10,35 @@ namespace algorithm {
 // Binary Indexed Tree 2D.
 template <typename T>
 class BIT2D {
-    int h, w;
-    std::vector<std::vector<T> > dat;  // 1-based index.
+    int m_h, m_w;
+    std::vector<std::vector<T> > m_dat;  // 1-based index.
 
     void build() {
         for(int i = 1; i <= height(); ++i)
             for(int j = 1; j <= width(); ++j) {
                 int ni = i + (i & -i), nj = j + (j & -j);
                 bool flag1 = (ni <= height()), flag2 = (nj <= width());
-                if(flag1) dat[ni][j] += dat[i][j];
-                if(flag2) dat[i][nj] += dat[i][j];
-                if(flag1 and flag2) dat[ni][nj] -= dat[i][j];
+                if(flag1) m_dat[ni][j] += m_dat[i][j];
+                if(flag2) m_dat[i][nj] += m_dat[i][j];
+                if(flag1 and flag2) m_dat[ni][nj] -= m_dat[i][j];
             }
     }
 
 public:
     // constructor. O(H*W).
     BIT2D() : BIT2D(0, 0) {}
-    explicit BIT2D(size_t h_, size_t w_) : h(h_), w(w_), dat(h_ + 1, std::vector<T>(w_ + 1, 0)) {}
-    explicit BIT2D(const std::vector<std::vector<T> > &dat_)
-        : h(dat_.size()), w(dat_[0].size()), dat(dat_.size() + 1, std::vector<T>(dat_[0].size() + 1)) {
-        for(int i = 0; i < height(); ++i) std::copy(dat_[i].begin(), dat_[i].end(), dat[i + 1].begin() + 1);
+    explicit BIT2D(size_t h, size_t w) : m_h(h), m_w(w), m_dat(h + 1, std::vector<T>(w + 1, 0)) {}
+    explicit BIT2D(const std::vector<std::vector<T> > &dat)
+        : m_h(dat.size()), m_w(dat[0].size()), m_dat(dat.size() + 1, std::vector<T>(dat[0].size() + 1)) {
+        for(int i = 0; i < height(); ++i) std::copy(dat[i].begin(), dat[i].end(), m_dat[i + 1].begin() + 1);
         build();
     }
 
-    int height() const { return h; }
-    int width() const { return w; }
+    int height() const { return m_h; }
+    int width() const { return m_w; }
     // 全要素をaで埋める．O(H*W).
     void fill(T a = 0) {
-        for(int i = 1; i <= height(); ++i) std::fill(dat[i].begin() + 1, dat[i].end(), a);
+        for(int i = 1; i <= height(); ++i) std::fill(m_dat[i].begin() + 1, m_dat[i].end(), a);
         if(a == 0) return;
         build();
     }
@@ -47,7 +47,7 @@ public:
         assert(1 <= y and y <= height());
         assert(1 <= x and x <= width());
         for(int i = y; i <= height(); i += (i & -i)) {
-            for(int j = x; j <= width(); j += (j & -j)) dat[i][j] += a;
+            for(int j = x; j <= width(); j += (j & -j)) m_dat[i][j] += a;
         }
     }
     // 区間[1,y]かつ[1,x]の総和を求める．O((logH)*logW).
@@ -56,7 +56,7 @@ public:
         assert(0 <= x and x <= width());
         T res = 0;
         for(int i = y; i > 0; i -= (i & -i)) {
-            for(int j = x; j > 0; j -= (j & -j)) res += dat[i][j];
+            for(int j = x; j > 0; j -= (j & -j)) res += m_dat[i][j];
         }
         return res;
     }
--- src/DataStructure/binary_indexed_tree_2d_zero.hpp
+++ src/DataStructure/binary_indexed_tree_2d_zero.hpp
@@ -10,17 +10,17 @@ namespace algorithm {
 // Binary Indexed Tree 2D (0-based index).
 template <typename T>
 class BIT2D0 {
-    int h, w;
-    std::vector<std::vector<T> > dat;  // 0-based index.
+    int m_h, m_w;
+    std::vector<std::vector<T> > m_dat;  // 0-based index.
 
     void build() {
         for(int i = 0; i < height(); ++i) {
             for(int j = 0; j < width(); ++j) {
                 int ni = i | (i + 1), nj = j | (j + 1);
                 bool flag1 = (ni < height()), flag2 = (nj < width());
-                if(flag1) dat[ni][j] += dat[i][j];
-                if(flag2) dat[i][nj] += dat[i][j];
-                if(flag1 and flag2) dat[ni][nj] -= dat[i][j];
+                if(flag1) m_dat[ni][j] += m_dat[i][j];
+                if(flag2) m_dat[i][nj] += m_dat[i][j];
+                if(flag1 and flag2) m_dat[ni][nj] -= m_dat[i][j];
             }
         }
     }
@@ -28,16 +28,16 @@ class BIT2D0 {
 public:
     // constructor. O(H*W).
     BIT2D0() : BIT2D0(0, 0) {}
-    explicit BIT2D0(size_t h_, size_t w_) : h(h_), w(w_), dat(h_, std::vector<T>(w_, 0)) {}
-    explicit BIT2D0(const std::vector<std::vector<T> > &dat_) : h(dat_.size()), w(dat_[0].size()), dat(dat_) {
+    explicit BIT2D0(size_t h, size_t w) : m_h(h), m_w(w), m_dat(h, std::vector<T>(w, 0)) {}
+    explicit BIT2D0(const std::vector<std::vector<T> > &dat) : m_h(dat.size()), m_w(dat[0].size()), m_dat(dat) {
         build();
     }
 
-    int height() const { return h; }
-    int width() const { return w; }
+    int height() const { return m_h; }
+    int width() const { return m_w; }
     // 全要素をaで埋める．O(H*W).
     void fill(T a = 0) {
-        for(int i = 0; i < height(); ++i) std::fill(dat[i].begin(), dat[i].end(), a);
+        for(int i = 0; i < height(); ++i) std::fill(m_dat[i].begin(), m_dat[i].end(), a);
         if(a == 0) return;
         build();
     }
@@ -46,7 +46,7 @@ public:
         assert(0 <= y and y < height());
         assert(0 <= x and x < width());
         for(int i = y; i < height(); i |= i + 1) {
-            for(int j = x; j < width(); j |= j + 1) dat[i][j] += a;
+            for(int j = x; j < width(); j |= j + 1) m_dat[i][j] += a;
         }
     }
     // 区間[0,y)かつ[0,x)の総和を求める．O((logH)*logW).
@@ -55,7 +55,7 @@ public:
         assert(0 <= x and x <= width());
         T res = 0;
         for(int i = y - 1; i >= 0; i = (i & (i + 1)) - 1) {
-            for(int j = x - 1; j >= 0; j = (j & (j + 1)) - 1) res += dat[i][j];
+            for(int j = x - 1; j >= 0; j = (j & (j + 1)) - 1) res += m_dat[i][j];
         }
         return res;
     }
