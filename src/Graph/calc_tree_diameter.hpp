27de611223a8757b13f703f32ce2e6e857f0c98f
Today <ipp.15122098@gmail.com>
2023-08-07 18:30:24 +0900

    [add] Create a function, "calc_tree_diameter()"

---
+++ src/Graph/calc_tree_diameter.hpp
@@ -0,0 +1,73 @@
+#ifndef ALGORITHM_CALC_TREE_DIAMETER_HPP
+#define ALGORITHM_CALC_TREE_DIAMETER_HPP 1
+
+#include <cassert>
+#include <queue>
+#include <utility>
+#include <vector>
+
+namespace algorithm {
+
+// 木の直径．O(|E|*|V|).
+std::tuple<int, int, int> calc_tree_diameter(const std::vector<std::vector<int> > &g) {
+    if(g.size() == 0) return {-1, -1, -1};
+    int endpoint;
+    std::vector<int> dist(g.size());
+    auto bfs = [&](int s) -> void {
+        std::queue<int> que;
+        que.push(s);
+        endpoint = s;
+        std::fill(dist.begin(), dist.end(), -1);
+        dist[s] = 0;
+        while(!que.empty()) {
+            auto u = que.front();
+            que.pop();
+            endpoint = u;
+            for(auto v : g[u]) {
+                if(dist[v] == -1) {
+                    dist[v] = dist[u] + 1;
+                    que.push(v);
+                }
+            }
+        }
+    };
+    bfs(0);
+    int tmp = endpoint;
+    bfs(endpoint);
+    return {dist[endpoint], tmp, endpoint};  // tuple of (distance, endpoint1, endpoint2).
+}
+
+// 重み付き木の直径．O(|E|*log|V|).
+template <typename Type>
+std::tuple<Type, int, int> calc_tree_diameter(const std::vector<std::vector<std::pair<int, Type> > > &g) {
+    if(g.size() == 0) return {-1, -1, -1};
+    int endpoint;
+    std::vector<Type> dist(g.size());
+    auto bfs = [&](int s) -> void {
+        std::priority_queue<std::pair<Type, int> > pque;
+        pque.emplace(0, s);
+        endpoint = s;
+        std::fill(dist.begin(), dist.end(), -1);
+        dist[s] = 0;
+        while(!pque.empty()) {
+            auto [d, u] = pque.top();
+            pque.pop();
+            if(-d > dist[u]) continue;
+            endpoint = u;
+            for(const auto &[v, cost] : g[u]) {
+                if(dist[v] == -1) {
+                    dist[v] = dist[u] + cost;
+                    pque.emplace(-dist[v], v);
+                }
+            }
+        }
+    };
+    bfs(0);
+    int tmp = endpoint;
+    bfs(endpoint);
+    return {dist[endpoint], tmp, endpoint};  // tuple of (distance, endpoint1, endpoint2).
+}
+
+}  // namespace algorithm
+
+#endif
