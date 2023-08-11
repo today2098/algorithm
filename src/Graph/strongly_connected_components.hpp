72e608187180e7f4a06a6a31eb9593e596b15f6a
Today <ipp.15122098@gmail.com>
2023-08-11 17:26:25 +0900

    [clean] Refactoring "SCC"

--- src/Graph/strongly_connected_components.hpp
+++ src/Graph/strongly_connected_components.hpp
@@ -9,39 +9,34 @@ namespace algorithm {
 
 // 強連結成分分解 (SCC: Strongly Connected Components)．
 class SCC {
-    using Graph = std::vector<std::vector<int> >;
-
-    int m_vn;   // m_vn:=(頂点数).
-    Graph m_g;  // m_g[v][]:=(頂点vの隣接リスト).
-
-    int infinity() const { return 1e9; }
+    int m_vn;                            // m_vn:=(頂点数).
+    std::vector<std::vector<int> > m_g;  // m_g[v][]:=(頂点vの隣接リスト).
 
 public:
     SCC() : SCC(0) {}
     explicit SCC(int vn) : m_vn(vn), m_g(vn) {}
-    explicit SCC(const Graph &g) : m_vn(g.size()), m_g(g) {}
 
     // 頂点数を返す．
-    int size() const { return m_vn; }
+    int order() const { return m_vn; }
     // 有向辺を張る．
     void add_edge(int from, int to) {
-        assert(0 <= from and from < size());
-        assert(0 <= to and to < size());
+        assert(0 <= from and from < order());
+        assert(0 <= to and to < order());
         m_g[from].push_back(to);
     }
     // return pair of (# of SCCs, SCC id of each nodes).
     std::pair<int, std::vector<int> > scc() const {
-        int num_sccs = 0;             // num_sccs:=(SCCsの数).
-        std::vector<int> id(size());  // id[v]:=(頂点vが属するSCCのID).
+        int num_sccs = 0;               // num_sccs:=(SCCsの数).
+        std::vector<int> ids(order());  // ids[v]:=(頂点vが属するSCCのID).
         // ord[v]:=(DFS木における頂点vの行きがけ順序).
         // low[v]:=(DFS木において，頂点vから葉方向に0回以上，後退辺を高々1回用いて到達できる頂点wのord[w]の最小値).
-        std::vector<int> ord(size(), -1), low(size());
+        std::vector<int> ord(order(), -1), low(order());
         int now_ord = 0;
         std::stack<int> visited;
         auto dfs = [&](auto self, int u) -> void {
             ord[u] = low[u] = now_ord++;
             visited.push(u);
-            for(auto to : m_g[u]) {
+            for(int to : m_g[u]) {
                 if(ord[to] == -1) {
                     self(self, to);
                     low[u] = std::min(low[u], low[to]);
@@ -51,25 +46,25 @@ public:
             }
             if(low[u] == ord[u]) {
                 while(true) {
-                    auto v = visited.top();
+                    int v = visited.top();
                     visited.pop();
-                    ord[v] = infinity();
-                    id[v] = num_sccs;
+                    ord[v] = order();  // inf.
+                    ids[v] = num_sccs;
                     if(v == u) break;
                 }
                 num_sccs++;
             }
         };
-        for(int v = 0; v < size(); ++v) {
+        for(int v = 0; v < order(); ++v) {
             if(ord[v] == -1) dfs(dfs, v);
         }
-        return {num_sccs, id};
+        return {num_sccs, ids};
     }
     // 有向グラフを強連結成分分解する．O(|V|+|E|).
     std::vector<std::vector<int> > decompose() const {
-        const auto &&[num_sccs, id] = scc();
+        const auto &&[num_sccs, ids] = scc();
         std::vector<std::vector<int> > sccs(num_sccs);
-        for(int v = 0; v < size(); ++v) sccs[id[v]].push_back(v);
+        for(int v = 0; v < order(); ++v) sccs[ids[v]].push_back(v);
         return sccs;
     }
 };
