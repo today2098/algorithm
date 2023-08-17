84c75f49bbcc089074ba2bed7242f04ac7a078f8
Today <ipp.15122098@gmail.com>
2023-08-17 14:58:54 +0900

    [clean] Refactoring "lis()"

--- src/DP/lis.hpp
+++ src/DP/lis.hpp
@@ -3,25 +3,27 @@
 
 #include <algorithm>
 #include <cassert>
+#include <functional>
 #include <vector>
 
 namespace algorithm {
 
-// 最長増加部分列 (LIS: Longest Increasing Subsequence) の長さを求める．引数はSTLのシーケンスコンテナ．O(|A|*log|A|).
-template <class Sequence>
-int lis(const Sequence &a, bool is_strong = true) {
-    assert(a.size() > 0);
-    Sequence dp({a[0]});
-    for(int i = 1; i < a.size(); ++i) {
-        if(is_strong) {  // 狭義単調増加．
-            if(dp[dp.size() - 1] < a[i]) dp.push_back(a[i]);
-            else *lower_bound(dp.begin(), dp.end(), a[i]) = a[i];
-        } else {  // 広義単調増加．
-            if(dp[dp.size() - 1] <= a[i]) dp.push_back(a[i]);
-            else *upper_bound(dp.begin(), dp.end(), a[i]) = a[i];
-        }
+// 最長増加部分列 (LIS: Longest Increasing Subsequence) の長さを求める．O(N*logN).
+template <typename Type, typename Func = std::function<bool(const Type &, const Type &)> >
+std::vector<int> lis(
+    const std::vector<Type> &v,
+    const Func &comp = [](const Type &a, const Type &b) -> bool { return a < b; }) {
+    const int n = v.size();
+    std::vector<int> res(n);  // res[i]:=(v[:i+1]の最長増加部分列の長さ).
+    std::vector<Type> dp;
+    dp.reserve(n);
+    for(int i = 0; i < n; ++i) {
+        auto itr = std::lower_bound(dp.begin(), dp.end(), v[i], comp);
+        if(itr == dp.end()) dp.push_back(v[i]);
+        else *itr = v[i];
+        res[i] = dp.size();
     }
-    return dp.size();
+    return res;
 }
 
 }  // namespace algorithm
