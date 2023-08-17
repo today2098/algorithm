f6bd5d84e871d20028809986a1834a45ffa113e9
Today <ipp.15122098@gmail.com>
2023-08-17 15:26:53 +0900

    [clean] Refactoring "lcs()"

--- src/DP/lcs.hpp
+++ src/DP/lcs.hpp
@@ -7,12 +7,13 @@
 
 namespace algorithm {
 
-// 最長共通部分列 (LCS: Longest Common Subsequence)．引数はSTLのシーケンスコンテナ．O(|A|*|B|).
+// 最長共通部分列 (LCS: Longest Common Subsequence)．
+// 引数はSTLのシーケンスコンテナ．O(|A|*|B|).
 template <class Sequence>
 Sequence lcs(const Sequence &a, const Sequence &b) {
-    assert(a.size() > 0 and b.size() > 0);
     const int n = a.size(), m = b.size();
-    std::vector<std::vector<int> > dp(n + 1, std::vector<int>(m + 1, 0));  // dp[i][j]:=(a[:i]とb[:j]のLCSの長さ).
+    // dp[i][j]:=(a[:i]とb[:j]のLCSの長さ).
+    std::vector<std::vector<int> > dp(n + 1, std::vector<int>(m + 1, 0));
     for(int i = 1; i <= n; ++i) {
         for(int j = 1; j <= m; ++j) {
             if(a[i - 1] == b[j - 1]) dp[i][j] = dp[i - 1][j - 1] + 1;
