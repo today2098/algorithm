99264fe56145ad440b530fef2eb84e868e2e8355
Today <ipp.15122098@gmail.com>
2023-08-28 17:01:09 +0900

    [update] Optimize "calc_tree_diameter()"

--- src/Graph/calc_tree_diameter.hpp
+++ src/Graph/calc_tree_diameter.hpp
@@ -9,64 +9,64 @@
 
 namespace algorithm {
 
-// 木の直径．O(|V|+|E|).
+// 木の直径を求める．O(|V|).
 std::tuple<int, int, int> calc_tree_diameter(const std::vector<std::vector<int> > &g) {
     if(g.size() == 0) return {-1, -1, -1};
     int endpoint;
-    std::vector<int> dist(g.size());
+    std::vector<int> d(g.size());
+    std::queue<int> que;
     auto bfs = [&](int s) -> void {
         endpoint = s;
-        std::fill(dist.begin(), dist.end(), -1);
-        dist[s] = 0;
-        std::queue<int> que;
+        std::fill(d.begin(), d.end(), -1);
+        d[s] = 0;
         que.push(s);
         while(!que.empty()) {
             int u = que.front();
             que.pop();
             endpoint = u;
             for(int v : g[u]) {
-                if(dist[v] == -1) {
-                    dist[v] = dist[u] + 1;
-                    que.push(v);
-                }
+                if(d[v] != -1) continue;
+                d[v] = d[u] + 1;
+                que.push(v);
             }
         }
     };
     bfs(0);
     int tmp = endpoint;
     bfs(endpoint);
-    return {dist[endpoint], tmp, endpoint};  // tuple of (distance, endpoint1, endpoint2).
+    return {d[endpoint], tmp, endpoint};  // tuple of (diameter, endpoint1, endpoint2).
 }
 
-// 重み付き木の直径．O(|E|*log|V|).
+// 重み付き木の直径を求める．O(|V|).
 template <typename Type>
 std::tuple<Type, int, int> calc_tree_diameter(const std::vector<std::vector<std::pair<int, Type> > > &g) {
     if(g.size() == 0) return {-1, -1, -1};
     int endpoint;
-    std::vector<Type> dist(g.size());
+    Type diameter;
+    std::vector<Type> d(g.size());
+    std::vector<bool> seen(g.size());
+    std::queue<int> que;
     auto bfs = [&](int s) -> void {
         endpoint = s;
-        std::fill(dist.begin(), dist.end(), -1);
-        dist[s] = 0;
-        std::priority_queue<std::pair<Type, int> > pque;
-        pque.emplace(0, s);
-        while(!pque.empty()) {
-            auto [d, u] = pque.top();
-            pque.pop();
-            if(-d > dist[u]) continue;
-            endpoint = u;
+        diameter = d[s] = 0;
+        std::fill(seen.begin(), seen.end(), false);
+        que.push(s);
+        while(!que.empty()) {
+            int u = que.top();
+            que.pop();
+            seen[u] = true;
+            if(d[u] > diameter) endpoint = u, diameter = d[u];
             for(const auto &[v, cost] : g[u]) {
-                if(dist[v] == -1) {
-                    dist[v] = dist[u] + cost;
-                    pque.emplace(-dist[v], v);
-                }
+                if(seen[v]) continue;
+                d[v] = d[u] + cost;
+                que.emplace(v);
             }
         }
     };
     bfs(0);
     int tmp = endpoint;
     bfs(endpoint);
-    return {dist[endpoint], tmp, endpoint};  // tuple of (distance, endpoint1, endpoint2).
+    return {diameter, tmp, endpoint};  // tuple of (diameter, endpoint1, endpoint2).
 }
 
 }  // namespace algorithm
