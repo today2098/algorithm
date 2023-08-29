c52b86a84f9ebb8d145de2340b11221585e33392
Today <ipp.15122098@gmail.com>
2023-08-29 13:04:56 +0900

    [add] Test about "prime_factorization()"

---
+++ test/aoj-NTL_1_A.test.cpp
@@ -0,0 +1,18 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/6/NTL/all/NTL_1_A"
+
+#include <iostream>
+
+#include "../src/MathNumberTheory/prime_factorization.hpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    auto mp = algorithm::prime_factorization(n);
+
+    std::cout << n << ":";
+    for(const auto &[p, cnt] : mp) {
+        for(int i = 0; i < cnt; ++i) std::cout << " " << p;
+    }
+    std::cout << std::endl;
+}
