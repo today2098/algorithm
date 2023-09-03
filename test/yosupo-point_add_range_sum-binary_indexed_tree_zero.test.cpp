69a54e06d6eb44c9c4aba93dab4de397834c1e2f
Today <ipp.15122098@gmail.com>
2023-09-03 16:17:58 +0900

    [add] Test "BIT0"

---
+++ test/yosupo-point_add_range_sum-binary_indexed_tree_zero.test.cpp
@@ -0,0 +1,34 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/point_add_range_sum"
+
+#include <iostream>
+#include <vector>
+
+#include "../src/DataStructure/SegmentTree/binary_indexed_tree_zero.hpp"
+
+int main() {
+    int n;
+    int q;
+    std::cin >> n >> q;
+
+    std::vector<long long> a(n);
+    for(auto &in : a) std::cin >> in;
+
+    algorithm::BIT0 bitree(a);
+    while(q--) {
+        int t;
+        std::cin >> t;
+
+        if(t == 0) {
+            int p;
+            long long x;
+            std::cin >> p >> x;
+
+            bitree.add(p, x);
+        } else {
+            int l, r;
+            std::cin >> l >> r;
+
+            std::cout << bitree.sum(l, r) << std::endl;
+        }
+    }
+}
