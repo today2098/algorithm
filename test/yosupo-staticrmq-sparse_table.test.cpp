f0232b4fdb325910aa3516b5fd617ad0a656f046
Today <ipp.15122098@gmail.com>
2023-09-03 21:55:05 +0900

    [add] Create doc and test "SparseTable"

---
+++ test/yosupo-staticrmq-sparse_table.test.cpp
@@ -0,0 +1,24 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/staticrmq"
+
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+#include "../src/DataStructure/SegmentTree/sparse_table.hpp"
+
+int main() {
+    int n;
+    int q;
+    std::cin >> n >> q;
+
+    std::vector<int> a(n);
+    for(auto &in : a) std::cin >> in;
+
+    algorithm::SparseTable<int> sparse_table([](int a, int b) -> int { return std::min(a, b); }, a);
+    while(q--) {
+        int l, r;
+        std::cin >> l >> r;
+
+        std::cout << sparse_table.fold(l, r) << std::endl;
+    }
+}
