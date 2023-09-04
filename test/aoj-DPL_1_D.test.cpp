14599b94cb8aef46721ff8a015108fad5a2daf47
Today <ipp.15122098@gmail.com>
2023-09-04 20:37:10 +0900

    [add] Test about "lis()"

---
+++ test/aoj-DPL_1_D.test.cpp
@@ -0,0 +1,17 @@
+#define PROBLEM "https://onlinejudge.u-aizu.ac.jp/courses/library/7/DPL/all/DPL_1_D"
+
+#include <iostream>
+#include <vector>
+
+#include "../src/DP/longest_increasing_subsequence.hpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> a(n);
+    for(auto &in : a) std::cin >> in;
+
+    auto &&lis = algorithm::lis(a);
+    std::cout << lis[n] << std::endl;
+}
