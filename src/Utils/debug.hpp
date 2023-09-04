4f2aca40c7fcb9adc5d02a1b57dc53f2e0ce5036
Today <ipp.15122098@gmail.com>
2023-09-04 16:46:00 +0900

    [add] Create doc and update about debug macro

--- src/Utils/debug.hpp
+++ src/Utils/debug.hpp
@@ -1,8 +1,16 @@
+/**
+ * @docs docs/Utils/debug.md
+ */
+
 #ifndef ALGORITHM_DEBUG_HPP
 #define ALGORITHM_DEBUG_HPP 1
 
 #include <iostream>
+#include <iterator>
+#include <queue>
+#include <stack>
 #include <string>
+#include <tuple>
 #include <type_traits>
 #include <utility>
 
@@ -16,25 +24,34 @@ namespace algorithm {
 
 namespace debug {
 
+constexpr std::ostream &os = std::cerr;
+
 struct has_iterator_impl {
     template <class T>
-    static std::true_type check(typename T::iterator *);
+    static constexpr std::true_type check(typename T::iterator *);
 
     template <class T>
-    static std::false_type check(...);
+    static constexpr std::false_type check(...);
 };
 
 template <class T>
 class has_iterator : public decltype(has_iterator_impl::check<T>(nullptr)) {};
 
-// Prototype declaration
+// Prototype declaration.
 void print(const std::string &s);
+void print(const std::string_view &s);
 template <typename T, typename U>
 void print(const std::pair<T, U> &p);
 template <class T, std::size_t... Idx>
 void print_tuple(const T &t, std::index_sequence<Idx...>);
 template <typename... T>
 void print(const std::tuple<T...> &t);
+template <typename T>
+void print(const std::stack<T> &st);
+template <typename T>
+void print(const std::queue<T> &que);
+template <typename T, typename... U>
+void print(const std::priority_queue<T, U...> &pque);
 template <typename T, typename std::enable_if_t<has_iterator<T>::value, bool> = false>
 void print(const T &v);
 template <typename T, typename std::enable_if_t<!has_iterator<T>::value, bool> = false>
@@ -42,22 +59,24 @@ void print(const T &elem);
 template <typename T, typename... Args>
 void debug_internal(int l, std::string_view context, T &&first, Args &&...args);
 
-void print(const std::string &s) { std::cerr << s; }
+void print(const std::string &s) { os << s; }
+
+void print(const std::string_view &s) { os << s; }
 
 template <typename T, typename U>
 void print(const std::pair<T, U> &p) {
-    std::cerr << "{";
+    os << "{";
     print(p.first);
-    std::cerr << ", ";
+    os << ", ";
     print(p.second);
-    std::cerr << "}";
+    os << "}";
 }
 
 template <class T, std::size_t... Idx>
 void print_tuple(const T &t, std::index_sequence<Idx...>) {
-    std::cerr << "{";
-    ((std::cerr << (Idx == 0 ? "" : ", "), print(std::get<Idx>(t))), ...);
-    std::cerr << "}";
+    os << "{";
+    ((os << (Idx == 0 ? "" : ", "), print(std::get<Idx>(t))), ...);
+    os << "}";
 }
 
 template <typename... T>
@@ -65,27 +84,66 @@ void print(const std::tuple<T...> &t) {
     print_tuple(t, std::make_index_sequence<sizeof...(T)>());
 }
 
+template <typename T>
+void print(const std::stack<T> &st) {
+    std::stack<T> tmp = st;
+    os << "[";
+    while(!tmp.empty()) {
+        T elem = tmp.top();
+        tmp.pop();
+        print(elem);
+        if(!tmp.empty()) os << " ";
+    }
+    os << "]";
+}
+
+template <typename T>
+void print(const std::queue<T> &que) {
+    std::queue<T> tmp = que;
+    os << "[";
+    while(!tmp.empty()) {
+        T elem = tmp.front();
+        tmp.pop();
+        print(elem);
+        if(!tmp.empty()) os << " ";
+    }
+    os << "]";
+}
+
+template <typename T, typename... U>
+void print(const std::priority_queue<T, U...> &pque) {
+    std::priority_queue<T, U...> tmp = pque;
+    os << "[";
+    while(!tmp.empty()) {
+        T elem = tmp.top();
+        tmp.pop();
+        print(elem);
+        if(!tmp.empty()) os << " ";
+    }
+    os << "]";
+}
+
 template <class T, typename std::enable_if_t<has_iterator<T>::value, bool> = false>
 void print(const T &v) {
-    std::cerr << "[";
+    os << "[";
     for(auto itr = std::cbegin(v); itr != std::cend(v); ++itr) {
-        if(itr != std::cbegin(v)) std::cerr << " ";
+        if(itr != std::cbegin(v)) os << " ";
         print(*itr);
     }
-    std::cerr << "]";
+    os << "]";
 }
 
 template <typename T, typename std::enable_if_t<!has_iterator<T>::value, bool> = false>
-void print(const T &elem) { std::cerr << elem; }
+void print(const T &elem) { os << elem; }
 
 template <typename T, typename... Args>
 void debug_internal(int l, std::string_view context, T &&first, Args &&...args) {
     constexpr const char *open_bracket = (sizeof...(args) == 0 ? "" : "(");
     constexpr const char *close_bracket = (sizeof...(args) == 0 ? "" : ")");
-    std::cerr << "[L" << l << "] " << open_bracket << context << close_bracket << ": " << open_bracket;
+    os << "[L" << l << "] " << open_bracket << context << close_bracket << ": " << open_bracket;
     print(std::forward<T>(first));
-    ((std::cerr << ", ", print(std::forward<Args>(args))), ...);
-    std::cerr << close_bracket << std::endl;
+    ((os << ", ", print(std::forward<Args>(args))), ...);
+    os << close_bracket << std::endl;
 }
 
 }  // namespace debug
@@ -99,10 +157,3 @@ void debug_internal(int l, std::string_view context, T &&first, Args &&...args) 
 }  // namespace algorithm
 
 #endif
-
-/*
-参考文献：
-- 競技プログラミングで print デバッグ，https://naskya.net/post/0002/（参照2023.3.1）．
-- EnumHack, C++メタ関数のまとめ，Qiita, https://qiita.com/_EnumHack/items/ee2141ad47915c55d9cb（参照2023.3.1）．
-- terukazu, 特定のメンバ関数有無で、呼び出す関数を変えたい，Qiita, https://qiita.com/terukazu/items/e257c05a7b191d32c577（参照2023.3.1）．
-*/
