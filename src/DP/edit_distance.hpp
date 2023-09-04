54f851c30b9fc2c58a2bebd8deeebcd1c684a83d
Today <ipp.15122098@gmail.com>
2023-09-04 21:09:09 +0900

    [clean] Refactoring

--- src/DP/edit_distance.hpp
+++ src/DP/edit_distance.hpp
@@ -1,3 +1,7 @@
+/**
+ * @brief Edit Distance（編集距離）
+ */
+
 #ifndef ALGORITHM_EDIT_DISTANCE_HPP
 #define ALGORITHM_EDIT_DISTANCE_HPP 1
 
@@ -6,20 +10,23 @@
 
 namespace algorithm {
 
-// 編集距離 (edit distance)．引数はSTLのシーケンスコンテナ．O(|S|*|T|).
+// Edit Distance（編集距離）.
+// 引数はSTLのシーケンスコンテナであること．O(|S|*|T|).
 template <class Sequence>
-int edit_distance(const Sequence &s, const Sequence &t) {
-    const int m = s.size(), n = t.size();
-    std::vector<std::vector<int> > dp(m + 1, std::vector<int>(n + 1));  // dp[i][j]:=(s[:i]とt[:j]の編集距離).
-    dp[0][0] = 0;
-    for(int i = 1; i <= m; ++i) dp[i][0] = i;
-    for(int j = 1; j <= n; ++j) dp[0][j] = j;
-    for(int i = 1; i <= m; ++i) {
-        for(int j = 1; j <= n; ++j) {
-            dp[i][j] = std::min({dp[i - 1][j] + 1, dp[i][j - 1] + 1, dp[i - 1][j - 1] + (s[i - 1] == t[j - 1] ? 0 : 1)});
+std::vector<std::vector<int> > edit_distance(const Sequence &s, const Sequence &t) {
+    const int n = s.size(), m = t.size();
+    // dp[i][j]:=(s[:i]とt[:j]の編集距離).
+    std::vector<std::vector<int> > dp(n + 1, std::vector<int>(m + 1, 0));
+    for(int i = 1; i <= n; ++i) dp[i][0] = i;
+    for(int j = 1; j <= m; ++j) dp[0][j] = j;
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= m; ++j) {
+            dp[i][j] = std::min({dp[i - 1][j] + 1,
+                                 dp[i][j - 1] + 1,
+                                 dp[i - 1][j - 1] + (s[i - 1] == t[j - 1] ? 0 : 1)});
         }
     }
-    return dp[m][n];
+    return dp;
 }
 
 }  // namespace algorithm
