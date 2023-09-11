23dd334896154cc9669107ac583ca5654b9de03d
Today <ipp.15122098@gmail.com>
2023-09-11 19:21:04 +0900

    [clean] Rename a function name

--- src/Math/NumberTheory/prime_factorization.hpp
+++ src/Math/NumberTheory/prime_factorization.hpp
@@ -2,8 +2,8 @@
  * @brief 素因数分解
  */
 
-#ifndef ALGORITHM_PRIME_FACTORIZATION_HPP
-#define ALGORITHM_PRIME_FACTORIZATION_HPP 1
+#ifndef ALGORITHM_PRIME_FACTORIZE_HPP
+#define ALGORITHM_PRIME_FACTORIZE_HPP 1
 
 #include <cassert>
 #include <map>
@@ -12,7 +12,7 @@ namespace algorithm {
 
 // 素因数分解．O(√N).
 template <typename Type>
-std::map<Type, int> prime_factorization(Type n) {
+std::map<Type, int> prime_factorize(Type n) {
     assert(n >= 2);
     std::map<Type, int> res;  // res[p]:=(自然数nに含まれる素因数pの個数).
     for(Type p = 2; p * p <= n; ++p) {
