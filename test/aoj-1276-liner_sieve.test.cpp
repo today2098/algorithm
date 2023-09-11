753c61bb80835ad3851be920119b24d9873f90a3
Today <ipp.15122098@gmail.com>
2023-09-11 18:10:17 +0900

    [add] Test about sieve

--- test/aoj-1276-liner_sieve.test.cpp
+++ test/aoj-1276-liner_sieve.test.cpp
@@ -3,19 +3,12 @@
 #include <algorithm>
 #include <iostream>
 #include <iterator>
-#include <vector>
 
-#include "../src/Math/NumberTheory/sieve.hpp"
+#include "../src/Math/NumberTheory/liner_sieve.hpp"
 
 int main() {
     constexpr int MX = 1299709;
-    algorithm::Sieve sieve(MX);
-
-    std::vector<int> primes;
-    primes.reserve(100000);
-    for(int p = 2; p <= MX; ++p) {
-        if(sieve.is_prime(p)) primes.push_back(p);
-    }
+    algorithm::LinearSieve sieve(MX);
 
     while(true) {
         int a;
@@ -23,7 +16,7 @@ int main() {
 
         if(a == 0) break;
 
-        auto itr = std::lower_bound(primes.cbegin(), primes.cend(), a);
+        auto itr = std::lower_bound(sieve.primes().cbegin(), sieve.primes().cend(), a);
         std::cout << (*itr == a ? 0 : *itr - *std::prev(itr)) << "\n";
     }
 }
