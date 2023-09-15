5252532e3c6ddaaab8f295c535c3fdbbe02f769c
Today <ipp.15122098@gmail.com>
2023-09-15 20:30:15 +0900

    [add] Create two function, "extgcd()" and "crt()"

---
+++ src/Math/NumberTheory/chinese_remainder_theorem.hpp
@@ -0,0 +1,47 @@
+/**
+ * @brief Chinese Remainder Theorem（中国剰余定理）
+ * @docs docs/Math/NumberTheory/chinese_remainder_theorem
+ */
+
+#ifndef ALGORITHM_CHINESE_REMAINDER_THEOREM_HPP
+#define ALGORITHM_CHINESE_REMAINDER_THEOREM_HPP 1
+
+#include <cassert>
+#include <vector>
+
+#include "extgcd.hpp"
+
+namespace algorithm {
+
+// Chinese Remainder Theorem（中国剰余定理）.
+// x≡b1 (mod m1) かつ x≡b2 (mod m2) を満たす整数x (0≦x＜lcm(m1,m2))を求める．
+// xが存在する必要十分条件は b1≡b2 (mod gcd(m1,m2)) であること．
+// 返り値はpair of (x,lcm(m1,m2))．解なしの場合，(0,-1)を返す．
+std::pair<long long, long long> crt(long long b1, long long m1, long long b2, long long m2) {
+    long long p, q;
+    long long d = extgcd(m1, m2, p, q);     // p is inverse of m1/d (mod m2/d).
+    if((b2 - b1) % d != 0) return {0, -1};  // 解なし．
+    long long m = m1 / d * m2;              // lcm(m1,m2).
+    long long tmp = (b2 - b1) / d * p % (m2 / d);
+    long long r = (b1 + m1 * tmp) % m;
+    if(r < 0) r += m;
+    return {r, m};
+}
+
+// Chinese Remainder Theorem（中国剰余定理）.
+// 各iについて，x≡b[i] (mod m[i]) を満たす整数x (0≦x＜lcm(m[]))を求める．
+// 返り値はpair of (x,lcm(m[]))．解なしの場合，(0,-1)を返す．
+std::pair<long long, long long> crt(const std::vector<long long> &bs, const std::vector<long long> &ms) {
+    assert(bs.size() == ms.size());
+    long long r = 0, m = 1;
+    for(int i = 0, n = bs.size(); i < n; ++i) {
+        const auto &&[nr, nm] = crt(r, m, bs[i], ms[i]);
+        if(nr == 0 and nm == -1) return {0, -1};  // 解なし．
+        r = nr, m = nm;
+    }
+    return {r, m};
+}
+
+}  // namespace algorithm
+
+#endif
--- src/Math/NumberTheory/extgcd.hpp
+++ src/Math/NumberTheory/extgcd.hpp
@@ -1,13 +1,15 @@
+/**
+ * @brief 拡張ユークリッドの互除法
+ * @docs docs/Math/NumberTheory/extgcd.md
+ */
+
 #ifndef ALGORITHM_EXTGCD_HPP
 #define ALGORITHM_EXTGCD_HPP 1
-
-#include <cassert>
-#include <vector>
 
 namespace algorithm {
 
 // 拡張ユークリッドの互除法．
-// ax+by=GCD(a,b) を満たす整数の組(x,y)を求め，GCD(a,b)を返す．O(logN).
+// ax+by=gcd(a,b) を満たす整数の組(x,y)を求め，gcd(a,b)を返す．O(logN).
 template <typename Type>
 Type extgcd(Type a, Type b, Type &x, Type &y) {
     if(b == 0) {
@@ -19,40 +21,6 @@ Type extgcd(Type a, Type b, Type &x, Type &y) {
     return d;
 }
 
-// 中国剰余定理 (Chinese Remainder Theorem)．
-// x≡b1 (mod. m1) かつ x≡b2 (mod. m2) を満たす整数x(0≦x＜lcm(m1,m2))を求める．
-// 返り値は x≡r (mod. m) となる組(r,m)．解なしの場合，(0,-1)を返す．
-std::pair<long long, long long> crt(long long b1, long long m1, long long b2, long long m2) {
-    long long p, q;
-    long long d = extgcd(m1, m2, p, q);  // p is inv of m1/d (mod. m2/d)
-    if((b2 - b1) % d != 0) return {0, -1};
-    long long m = m1 / d * m2;  // lcm of (m1,m2)
-    long long tmp = (b2 - b1) / d * p % (m2 / d);
-    long long r = ((b1 + m1 * tmp) % m + m) % m;
-    return {r, m};
-}
-
-// 中国剰余定理 (Chinese Remainder Theorem)．
-// 各iについて，x≡b[i] (mod. m[i]) を満たす整数x(0≦x＜lcm(m[i]))を求める．
-// 返り値は x≡r (mod. m) となる組(r,m)．解なしの場合，(0,-1)を返す．
-std::pair<long long, long long> crt(const std::vector<long long> &bs, const std::vector<long long> &ms) {
-    assert(bs.size() == ms.size());
-    const int sz = bs.size();
-    long long r = 0, m = 1;
-    for(int i = 0; i < sz; ++i) {
-        auto &&[rr, mm] = crt(r, m, bs[i], ms[i]);
-        if(rr == 0 and mm == -1) return {0, -1};
-        r = rr, m = mm;
-    }
-    return {r, m};
-}
-
 }  // namespace algorithm
 
 #endif
-
-/*
-参考文献
-- drken, 拡張ユークリッドの互除法 〜 一次不定方程式 ax + by = c の解き方 〜，Qiita, https://qiita.com/drken/items/b97ff231e43bce50199a（参照2023.2.2）．
-- drken，中国剰余定理 (CRT) の解説とそれを用いる問題のまとめ，Qiita, https://qiita.com/drken/items/ae02240cd1f8edfc86fd（参照2022.9.16）．
-*/
