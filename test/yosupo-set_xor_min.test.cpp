01e68a182fb0e508e010d93b86836c6ea7a08e7b
Today <ipp.15122098@gmail.com>
2023-09-21 06:54:19 +0900

    [add] Refactoring and create doc and test about "BinaryTrie"

---
+++ test/yosupo-set_xor_min.test.cpp
@@ -0,0 +1,30 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/set_xor_min"
+
+#include <iostream>
+
+#include "../src/DataStructure/binary_trie.hpp"
+#include "../src/Utils/debug.hpp"
+
+int main() {
+    int q;
+    std::cin >> q;
+
+    algorithm::BinaryTrie<30> trie;
+    while(q--) {
+        int t;
+        int x;
+        std::cin >> t >> x;
+
+        if(t == 0) {
+            if(!trie.exist(x)) trie.insert(x);
+        } else if(t == 1) {
+            if(trie.exist(x)) trie.erase(x);
+        } else {
+            debug(trie);
+            trie.operator_xor(x);
+            debug(trie);
+            std::cout << trie.min_element().to_ulong() << "\n";
+            trie.operator_xor(x);
+        }
+    }
+}
