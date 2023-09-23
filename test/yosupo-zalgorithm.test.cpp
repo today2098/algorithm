65e8c609cf768c92b452b063e5d22d2275751aea
Today <ipp.15122098@gmail.com>
2023-09-23 23:34:18 +0900

    [add] Create doc and test about z-algorithm

---
+++ test/yosupo-zalgorithm.test.cpp
@@ -0,0 +1,16 @@
+#define PROBLEM "https://judge.yosupo.jp/problem/zalgorithm"
+
+#include <iostream>
+#include <string>
+
+#include "../src/String/z_algorithm.hpp"
+
+int main() {
+    std::string s;
+    std::cin >> s;
+
+    const auto &&z = algorithm::z_algorithm(s);
+
+    for(int i = 0, n = s.size(); i < n; ++i) std::cout << z[i] << " ";
+    std::cout << std::endl;
+}
