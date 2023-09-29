93010d2c65c5357e3819376030af9fef604308a6
Today <ipp.15122098@gmail.com>
2023-09-29 15:04:35 +0900

    [add] Update about "BigInteger"

--- src/Math/big_integer.hpp
+++ src/Math/big_integer.hpp
@@ -8,48 +8,86 @@
 
 #include <algorithm>
 #include <cassert>
+#include <cmath>
 #include <cstdint>
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <vector>
+
+#include "../Utils/debug.hpp"
 
 namespace algorithm {
 
 class BigInteger {
-    std::vector<int32_t> m_digit;
+    static constexpr int BASE_DIGIT = 8;
+    static constexpr int64_t BASE[BASE_DIGIT + 1] = {1, 10, 100, 1'000, 10'000, 100'000, 1'000'000, 10'000'000, 100'000'000};
+
+    std::vector<int64_t> m_digit;  // m_digit[k]:=(BASE_DIGIT*k+1桁目の数).
+
+    void convert(const std::string &s) {
+        if(s.empty()) {
+            m_digit.assign(1, 0);
+            return;
+        }
+        if(s[0] == '-') {
+            assert(s != "-");
+            const int n = s.size();
+            const int sz = (n - 1 + BASE_DIGIT - 1) / BASE_DIGIT;
+            m_digit.assign(sz, 0);
+            for(int i = 0; i < sz; ++i) {
+                int offset = BASE_DIGIT * i;
+                for(int j = 0; j < BASE_DIGIT and offset + j < n - 1; ++j) {
+                    assert('0' <= s[n - 1 - (offset + j)] and s[n - 1 - (offset + j)] <= '9');
+                    m_digit[i] -= BASE[j] * (s[n - 1 - (offset + j)] - '0');
+                }
+            }
+        } else {
+            const int n = s.size();
+            const int sz = (n + BASE_DIGIT - 1) / BASE_DIGIT;
+            m_digit.assign(sz, 0);
+            for(int i = 0; i < sz; ++i) {
+                int offset = BASE_DIGIT * i;
+                for(int j = 0; j < BASE_DIGIT and offset + j < n; ++j) {
+                    assert('0' <= s[n - 1 - (offset + j)] and s[n - 1 - (offset + j)] <= '9');
+                    m_digit[i] += BASE[j] * (s[n - 1 - (offset + j)] - '0');
+                }
+            }
+        }
+        while(m_digit.size() >= 2 and m_digit.back() == 0) m_digit.pop_back();
+    }
 
 public:
     BigInteger() : m_digit(1, 0) {}
-    explicit BigInteger(const std::string &s) : m_digit(s.size() + 1, 0) {
-        if(s.size() == 0) return;
-        assert(('0' <= s[0] and s[0] <= '9') or s[0] == '-');
-        const int n = s.size();
-        for(int i = 0; i < n - 1; ++i) {
-            assert('0' <= s[n - 1 - i] and s[n - 1 - i] <= '9');
-            m_digit[i] = s[n - 1 - i] - '0';
-        }
-        if(s[0] != '-') m_digit[n - 1] = s[0] - '0';
-        while(m_digit.size() >= 2 and m_digit.back() == 0) m_digit.pop_back();
-        if(s[0] == '-') m_digit.back() = -m_digit.back();
+    explicit BigInteger(int64_t a) : m_digit(1, a) {}
+    explicit BigInteger(const std::string &s) {
+        convert(s);
     }
-    explicit BigInteger(long long a) {
-        bool is_minus = false;
-        if(a < 0) is_minus = true, a = -a;
-        while(a > 0) {
-            m_digit.push_back(a % 10);
-            a /= 10;
-        }
-        if(m_digit.empty()) m_digit.assign(1, 0);
-        if(is_minus) m_digit.back() = -m_digit.back();
-    };
 
-    // 桁数を返す．O(1).
-    int size() const { return m_digit.size(); }
-
+    friend std::istream &operator>>(std::istream &is, BigInteger &rhs) {
+        std::string s;
+        is >> s;
+        rhs.convert(s);
+        return is;
+    }
     friend std::ostream &operator<<(std::ostream &os, const BigInteger &rhs) {
-        for(auto itr = rhs.m_digit.crbegin(); itr != rhs.m_digit.crend(); ++itr) os << *itr;
+        os << rhs.m_digit.back();
+        for(auto itr = rhs.m_digit.crbegin() + 1; itr < rhs.m_digit.crend(); ++itr) os << std::setfill('0') << std::right << std::setw(BASE_DIGIT) << std::abs(*itr);
         return os;
     }
+
+    // 桁数を返す．
+    int digit_number() const {
+        int res = 1;
+        while(res < BASE_DIGIT and std::abs(m_digit.back()) >= BASE[res]) res++;
+        res += BASE_DIGIT * ((int)m_digit.size() - 1);
+        return res;
+    }
+    std::string to_string() const {
+        std::ostringstream oss;
+        oss << *this;
+        return oss.str();
+    };
 };
 
 }  // namespace algorithm
