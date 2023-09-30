98d094ad0a2cc98c2df40a74aa012f3f987b3b76
Today <ipp.15122098@gmail.com>
2023-10-01 01:36:35 +0900

    [add] Refactoring and create doc about "split()"

---
+++ docs/String/split.md
@@ -0,0 +1,9 @@
+## 概要
+
+他の言語によくある split 関数と同じ機能のもの．
+引数にある文字列を指定した文字または文字列で分割する．
+
+
+## 参考文献
+
+1. MaryCore. "C++ std::string 文字列の分割（split）｜区切り文字／文字列に対応". <https://marycore.jp/prog/cpp/std-string-split/>.
--- src/String/split.hpp
+++ src/String/split.hpp
@@ -1,38 +1,42 @@
 #ifndef ALGORITHM_SPLIT_HPP
 #define ALGORITHM_SPLIT_HPP 1
 
+/**
+ * @brief 文字列分割
+ * @docs docs/String/split.md
+ */
+
 #include <sstream>
 #include <string>
 #include <vector>
 
 namespace algorithm {
 
-// 文字列分割．指定の文字delimで分割する．
-std::vector<std::string> split(std::string s, char delim, bool erace_empty = true) {
-    s = s + std::string({delim});
+// 文字列分割．文字列sを指定の文字delimで分割する．
+std::vector<std::string> split(const std::string &s, char delim, bool disable_empty = false) {
     std::vector<std::string> res;
-    std::stringstream ss(s);
+    std::istringstream ss(s + delim);
     std::string item;
     while(std::getline(ss, item, delim)) {
-        if(!item.empty() or !erace_empty) res.push_back(item);
+        if(!(item.empty() and disable_empty)) res.push_back(item);
     }
     return res;
 }
 
-// 文字列分割．指定の文字列delimで分割する．
-std::vector<std::string> split(const std::string &s, const std::string &delim, bool erace_empty = true) {
+// 文字列分割．文字列sを指定の文字列delimで分割する．
+std::vector<std::string> split(const std::string &s, const std::string &delim, bool disable_empty = false) {
     std::vector<std::string> res;
-    const auto length = delim.size();
-    auto offset = std::string::size_type(0);
-    while(1) {
-        auto pos = s.find(delim, offset);
+    const size_t length = delim.size();
+    size_t offset = 0;
+    while(true) {
+        size_t pos = s.find(delim, offset);
         if(pos == std::string::npos) {
-            auto &&item = s.substr(offset);
-            if(!item.empty() or !erace_empty) res.push_back(item);
+            std::string &&item = s.substr(offset);
+            if(!(item.empty() and disable_empty)) res.push_back(item);
             break;
         }
-        auto &&item = s.substr(offset, pos - offset);
-        if(!item.empty() or !erace_empty) res.push_back(item);
+        std::string &&item = s.substr(offset, pos - offset);
+        if(!(item.empty() and disable_empty)) res.push_back(item);
         offset = pos + length;
     }
     return res;
@@ -41,8 +45,3 @@ std::vector<std::string> split(const std::string &s, const std::string &delim, b
 }  // namespace algorithm
 
 #endif
-
-/*
-参考文献
-- C++ std::string 文字列の分割（split）｜区切り文字／文字列に対応，MaryCore, https://marycore.jp/prog/cpp/std-string-split/（参照2023.1.21）.
-*/
