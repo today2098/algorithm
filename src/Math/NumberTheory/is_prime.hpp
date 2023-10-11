808474373bb6c2515423c3b97be12ff3f2bcc4ce
Today <ipp.15122098@gmail.com>
2023-10-11 18:58:00 +0900

    [update] Optimize "is_prime()" and "prime_factorize()"

--- src/Math/NumberTheory/is_prime.hpp
+++ src/Math/NumberTheory/is_prime.hpp
@@ -1,9 +1,10 @@
-/**
- * @brief 素数判定
- */
-
 #ifndef ALGORITHM_IS_PRIME_HPP
 #define ALGORITHM_IS_PRIME_HPP 1
+
+/**
+ * @brief 素数判定
+ * @docs docs/Math/NumberTheory/is_prime.md
+ */
 
 #include <cassert>
 
@@ -14,7 +15,9 @@ template <typename Type>
 bool is_prime(Type n) {
     assert(n >= 0);
     if(n < 2) return false;
-    for(Type p = 2; p * p <= n; ++p) {
+    if(n == 2) return true;
+    if(n % 2 == 0) return false;
+    for(Type p = 3; p * p <= n; p += 2) {
         if(n % p == 0) return false;
     }
     return true;
--- src/Math/NumberTheory/prime_factorize.hpp
+++ src/Math/NumberTheory/prime_factorize.hpp
@@ -1,9 +1,10 @@
-/**
- * @brief 素因数分解
- */
-
 #ifndef ALGORITHM_PRIME_FACTORIZE_HPP
 #define ALGORITHM_PRIME_FACTORIZE_HPP 1
+
+/**
+ * @brief 素因数分解
+ * @docs docs/Math/NumberTheory/prime_factorize.md
+ */
 
 #include <cassert>
 #include <map>
@@ -15,7 +16,8 @@ template <typename Type>
 std::map<Type, int> prime_factorize(Type n) {
     assert(n >= 0);
     std::map<Type, int> res;  // res[p]:=(自然数nに含まれる素因数pの個数).
-    for(Type p = 2; p * p <= n; ++p) {
+    while(n % 2 == 0) res[2]++, n /= 2;
+    for(Type p = 3; p * p <= n; p += 2) {
         while(n % p == 0) res[p]++, n /= p;
     }
     if(n > 1) res[n] = 1;
