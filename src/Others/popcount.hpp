1c1181d8d8b206ed014ae6310adbb566d3018c0d
Today <76273733+today2098@users.noreply.github.com>
2024-04-19 16:59:43 +0900

    [add] Create the docs about "popcount"

--- src/Others/popcount.hpp
+++ src/Others/popcount.hpp
@@ -1,20 +1,25 @@
 #ifndef ALGORITHM_POPCOUNT_HPP
 #define ALGORITHM_POPCOUNT_HPP 1
 
+/**
+ * @brief Population Count (Popcount)
+ * @docs docs/Others/popcount.md
+ */
+
 #include <cstdint>
 
 namespace algorithm {
 
 int popcount32(uint32_t n) {
     constexpr uint32_t masks[5] = {0x55555555U, 0x33333333U, 0x0f0f0f0fU, 0x00ff00ffU, 0x0000ffffU};
-    for(int i = 0; i < 5; ++i) n = (n & masks[i]) + ((n >> (1U << i)) & masks[i]);
+    for(int i = 0; i < 5; ++i) n = (n & masks[i]) + ((n >> (1 << i)) & masks[i]);
     return n;
 }
 
 int popcount64(uint64_t n) {
     constexpr uint64_t masks[6] = {0x5555555555555555ULL, 0x3333333333333333ULL, 0x0f0f0f0f0f0f0f0fULL,
                                    0x00ff00ff00ff00ffULL, 0x0000ffff0000ffffULL, 0x00000000ffffffffULL};
-    for(int i = 0; i < 6; ++i) n = (n & masks[i]) + ((n >> (1ULL << i)) & masks[i]);
+    for(int i = 0; i < 6; ++i) n = (n & masks[i]) + ((n >> (1 << i)) & masks[i]);
     return n;
 }
 
