88a448c4beb1c4a5e5574163a1003517d0e671c9
Today <76273733+today2098@users.noreply.github.com>
2024-05-27 09:52:40 +0900

    [clean] Refactoring about combinatorics

--- src/Math/Combinatorics/combination.hpp
+++ src/Math/Combinatorics/combination.hpp
@@ -1,86 +1,33 @@
 #ifndef ALGORITHM_COMBINATION_HPP
 #define ALGORITHM_COMBINATION_HPP 1
 
+/**
+ * @brief 順列，組合せ，重複組合せ
+ * @docs docs/Math/Combinatorics/combination.md
+ */
+
 #include <cassert>
-#include <vector>
 
 namespace algorithm {
 
-// 二項係数（mod付き）．
-template <int mod>
-class Combination {
-    int m_sz;                       // m_sz:=(要素数). 制約は m_sz<=mod．
-    std::vector<long long> m_fact;  // m_fact[n]:=(nの階乗).
-    std::vector<long long> m_inv;   // m_inv[n]:=(nの逆元).
-    std::vector<long long> m_finv;  // m_finv[n]:=(nの階乗の逆元).
-
-public:
-    // constructor. O(N).
-    Combination() : Combination(51e4) {}
-    explicit Combination(size_t sz) : m_sz(sz), m_fact(sz), m_inv(sz), m_finv(sz) {
-        static_assert(mod >= 1);
-        assert(2 <= m_sz and m_sz <= mod);
-        m_fact[0] = m_fact[1] = 1;
-        m_inv[1] = 1;
-        m_finv[0] = m_finv[1] = 1;
-        for(int n = 2; n < m_sz; ++n) {
-            m_fact[n] = m_fact[n - 1] * n % mod;
-            m_inv[n] = mod - m_inv[mod % n] * (mod / n) % mod;
-            m_finv[n] = m_finv[n - 1] * m_inv[n] % mod;
-        }
-    }
-
-    static constexpr int modulus() { return mod; }
-    // 階乗．
-    long long factorial(int n) const {
-        assert(0 <= n and n < m_sz);
-        return m_fact[n];
-    }
-    // 逆元．
-    long long inverse(int n) const {
-        assert(1 <= n and n < m_sz);
-        return m_inv[n];
-    }
-    // 階乗の逆元．
-    long long inverse_fact(int n) const {
-        assert(0 <= n and n < m_sz);
-        return m_finv[n];
-    }
-    // 順列．
-    long long nPk(int n, int k) const {
-        assert(0 <= k and k <= n and n < m_sz);
-        return m_fact[n] * m_finv[n - k] % mod;
-    }
-    // 組合せ．
-    long long nCk(int n, int k) const {
-        assert(0 <= k and k <= n and n < m_sz);
-        return m_fact[n] * m_finv[n - k] % mod * m_finv[k] % mod;
-    }
-    // 重複組合せ．
-    long long nHk(int n, int k) const { return nCk(k + n - 1, n - 1); }
-};
-
-using Combination998244353 = Combination<998'244'353>;
-using Combination1000000007 = Combination<1'000'000'007>;
-
-// 順列．
-inline long long nPk(long long n, int k) {
+// 順列．O(K).
+long long nPk(long long n, int k) {
     assert(0 <= k and k <= n);
     long long res = 1;
     for(int i = 0; i < k; ++i) res = res * (n - i);
     return res;
 }
 
-// 組合せ．
-inline long long nCk(long long n, int k) {
+// 組合せ．O(K).
+long long nCk(long long n, int k) {
     assert(0 <= k and k <= n);
     long long res = 1;
     for(int i = 0; i < k; ++i) res = res * (n - i) / (i + 1);
     return res;
 }
 
-// 重複組合せ．
-inline long long nHk(int n, long long k) { return nCk(k + n - 1, n - 1); }
+// 重複組合せ．O(N).
+long long nHk(int n, long long k) { return nCk(k + n - 1, n - 1); }
 
 }  // namespace algorithm
 
---
+++ src/Math/Combinatorics/combination_with_mod.hpp
@@ -0,0 +1,73 @@
+#ifndef ALGORITHM_COMBINATION_WITH_MOD_HPP
+#define ALGORITHM_COMBINATION_WITH_MOD_HPP 1
+
+/**
+ * @brief 組合せ（mod付き）
+ * @docs docs/Math/Combinatorics/combination_with_mod.md
+ */
+
+#include <cassert>
+#include <vector>
+
+namespace algorithm {
+
+// 組合せ（mod付き）．
+template <int mod>
+class Combination {
+    int m_mx;                       // m_mx:=(引数nの最大値). 制約は m_mx<mod．
+    std::vector<long long> m_fact;  // m_fact[n]:=(nの階乗).
+    std::vector<long long> m_inv;   // m_inv[n]:=(nの逆元).
+    std::vector<long long> m_finv;  // m_finv[n]:=(nの階乗の逆元).
+
+public:
+    // constructor. O(N).
+    Combination() : Combination(51e4) {}
+    explicit Combination(int mx) : m_mx(mx), m_fact(mx + 1), m_inv(mx + 1), m_finv(mx + 1) {
+        static_assert(mod >= 1);
+        assert(1 <= m_mx and m_mx < mod);
+        m_fact[0] = m_fact[1] = 1;
+        m_inv[1] = 1;
+        m_finv[0] = m_finv[1] = 1;
+        for(int n = 2; n <= m_mx; ++n) {
+            m_fact[n] = m_fact[n - 1] * n % mod;
+            m_inv[n] = mod - m_inv[mod % n] * (mod / n) % mod;
+            m_finv[n] = m_finv[n - 1] * m_inv[n] % mod;
+        }
+    }
+
+    static constexpr int modulus() { return mod; }
+    // 階乗．
+    long long factorial(int n) const {
+        assert(0 <= n and n <= m_mx);
+        return m_fact[n];
+    }
+    // 逆元．
+    long long inverse(int n) const {
+        assert(1 <= n and n <= m_mx);
+        return m_inv[n];
+    }
+    // 階乗の逆元．
+    long long inverse_fact(int n) const {
+        assert(0 <= n and n <= m_mx);
+        return m_finv[n];
+    }
+    // 順列．
+    long long nPk(int n, int k) const {
+        assert(0 <= k and k <= n and n <= m_mx);
+        return m_fact[n] * m_finv[n - k] % mod;
+    }
+    // 組合せ．
+    long long nCk(int n, int k) const {
+        assert(0 <= k and k <= n and n <= m_mx);
+        return m_fact[n] * m_finv[n - k] % mod * m_finv[k] % mod;
+    }
+    // 重複組合せ．
+    long long nHk(int n, int k) const { return nCk(k + n - 1, n - 1); }
+};
+
+using Combination998244353 = Combination<998'244'353>;
+using Combination1000000007 = Combination<1'000'000'007>;
+
+}  // namespace algorithm
+
+#endif
