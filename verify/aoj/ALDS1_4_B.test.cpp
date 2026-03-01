c75db9446848fb3c29e677b606b24df1fbe5ba61
Today <76273733+today2098@users.noreply.github.com>
2026-03-01 09:43:29 +0000

    [add] Implement binary search algorithms
    - Added integral and floating-point versions of the binary search function.
    - Created documentation for the binary search algorithm.
    - Added a test file to verify the functionality of the binary search implementation.

---
+++ verify/aoj/ALDS1_4_B.test.cpp
@@ -0,0 +1,30 @@
+// competitive-verifier: PROBLEM https://onlinejudge.u-aizu.ac.jp/courses/lesson/1/ALDS1/4/ALDS1_4_B
+#include <iostream>
+#include <vector>
+
+#include "../algorithm/Others/binary_search.hpp"
+
+int main() {
+    int n;
+    std::cin >> n;
+
+    std::vector<int> s(n);
+    for(auto &elem : s) std::cin >> elem;
+
+    int q;
+    std::cin >> q;
+
+    int ans = 0;
+    while(q--) {
+        int t;
+        std::cin >> t;
+
+        auto itr = algorithm::bisearch(0, n, [&](int i) -> bool {
+            return s[i] < t;
+        });
+
+        if(itr < n and s[itr] == t) ++ans;
+    }
+
+    std::cout << ans << std::endl;
+}
